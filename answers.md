6cdc04e86053721f27de2d1332a90b55fb427c40
vizualtxt <88392631+vizualtxt@users.noreply.github.com>
2023-12-13 02:21:37 +0000

    finishamundo

--- answers.md
+++ answers.md
@@ -2,8 +2,7 @@
 
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:** Ali Sulehria
 
 
 Place all written answers from `recitation-08.md` here for easier grading.
@@ -12,7 +11,9 @@ Place all written answers from `recitation-08.md` here for easier grading.
 
 - **1b)**
 
-
+  Work: Implements Dijkstra's algorithm, so W = O(|E|log(|E))
+  Span: Same as work (by Dijkstra's)
 
 - **2b)**
 
+  get_path(): completed in main.py
--- main.py
+++ main.py
@@ -2,30 +2,53 @@ from collections import deque
 from heapq import heappush, heappop 
 
 def shortest_shortest_path(graph, source):
-    """
-    Params: 
-      graph.....a graph represented as a dict where each key is a vertex
-                and the value is a set of (vertex, weight) tuples (as in the test case)
-      source....the source node
-      
-    Returns:
-      a dict where each key is a vertex and the value is a tuple of
-      (shortest path weight, shortest path number of edges). See test case for example.
-    """
-    ### TODO
-    pass
+  """
+  Params: 
+    graph.....a graph represented as a dict where each key is a vertex
+              and the value is a set of (vertex, weight) tuples (as in the test case)
+    source....the source node
     
+  Returns:
+    a dict where each key is a vertex and the value is a tuple of
+    (shortest path weight, shortest path number of edges). See test case for example.
+  """
+  visited = {}
+  frontier = []
+  heappush(frontier, (0, 0, source))  # Flatten the tuple structure
 
-    
-    
+  while frontier:
+      distance, edges, node = heappop(frontier)
+
+      if node not in visited or (distance < visited[node][0]):
+          visited[node] = (distance, edges)
+
+          for neighbor, weight in graph[node]:
+              if neighbor not in visited:
+                  heappush(frontier, (distance + weight, edges + 1, neighbor))
+
+  return visited
+
+
 def bfs_path(graph, source):
     """
     Returns:
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
-    ###TODO
-    pass
+    visited = set()
+    frontier = deque()
+    frontier.append(source)
+    parentset = {source: None}
+  
+    while frontier:
+        node = frontier.popleft()
+        for neighbor in graph[node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                parentset[neighbor] = node
+                frontier.append(neighbor)
+    return parentset
+    
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -44,5 +67,10 @@ def get_path(parents, destination):
       (excluding the destination node itself). See test_get_path for an example.
     """
     ###TODO
-    pass
+    path = []
+    current  = parents[destination]
+    while current:
+      path.append(current)
+      current = parents[current]
+    return ''.join(path[::-1])
 
